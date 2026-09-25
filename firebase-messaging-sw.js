e3635c309aa4a64c7fc2f0141f3dda3e86c87f9e
Rander <90118184+Rander1997@users.noreply.github.com>
2026-09-25 22:59:06 +0300

    Update Firebase configuration
    Updated Firebase configuration with new project details.

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -2,12 +2,13 @@ importScripts('https://www.gstatic.com/firebasejs/9.22.0/firebase-app-compat.js'
 importScripts('https://www.gstatic.com/firebasejs/9.22.0/firebase-messaging-compat.js');
 
 const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_PROJECT_ID.appspot.com",
-    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-    appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyA53KoefZuP2DkgzHXpHDNlb23DvUK503I",
+  authDomain: "rander-43b8f.firebaseapp.com",
+  projectId: "rander-43b8f",
+  storageBucket: "rander-43b8f.firebasestorage.app",
+  messagingSenderId: "45881930163",
+  appId: "1:45881930163:web:985cc23d7dac31c957dbc6",
+  measurementId: "G-EJGNEBYPR3"
 };
 
 firebase.initializeApp(firebaseConfig);
